0c5268cf435f34e4d28cb5732ddf94c8ead7696a
Applesauce <gutongshuo@gmail.com>
2026-02-24 21:08:13 +0800

    yeta

--- ham.js
+++ ham.js
@@ -5,3 +5,12 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
+
+
+//year
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
--- index.js
+++ index.js
@@ -49,7 +49,3 @@ dots.forEach((dot, index) => {
     });
 });
 
-//Year
-const yearSpan = document.getElementById("year");
-const currentYear = new Date().getFullYear();
-yearSpan.textContent = currentYear;
